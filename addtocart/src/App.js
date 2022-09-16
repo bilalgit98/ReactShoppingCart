4d946b7a0aeedf34902d5936ead9b6fbf0b44e74
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-09-16 20:30:32 +0100

    Main Components

--- addtocart/src/App.js
+++ addtocart/src/App.js
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Basket from "./components/Basket";
+import data from "./data";
 function App() {
+  const { products } = data;
   return (
     <div>
       <Header />
       <div className="row">
-        <Main />
+        <Main products={products} />
         <Basket />
       </div>
     </div>
