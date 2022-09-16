cdc4e89b1c54e6c1e5a74f2dbb26b6eb14e0b6ce
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-09-16 16:32:30 +0100

    Overall Layout and Setup

--- addtocart/src/App.js
+++ addtocart/src/App.js
@@ -1,9 +1,14 @@
-import "./App.css";
 import Header from "./components/Header";
+import Main from "./components/Main";
+import Basket from "./components/Basket";
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
+      <div className="row">
+        <Main />
+        <Basket />
+      </div>
     </div>
   );
 }
--- addtocart/src/components/Header.js
+++ addtocart/src/components/Header.js
@@ -1,42 +1,15 @@
-import React from "react";
-import {
-  Badge,
-  Container,
-  FormControl,
-  Nav,
-  Navbar,
-  Dropdown,
-} from "react-bootstrap";
-import { FaShoppingCart } from "react-icons/fa";
-
 const Header = () => {
   return (
-    <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
-      <Container>
-        <Navbar.Brand>
-          <a href="/">Shopping cart</a>
-        </Navbar.Brand>
-        <Navbar.Text className="search">
-          <FormControl
-            style={{ width: 500 }}
-            placeholder="Search a Product"
-            className="m-auto"
-          />
-        </Navbar.Text>
-        <Nav>
-          <Dropdown alignRight>
-            <Dropdown.Toggle variant="success">
-              <FaShoppingCart color="white" fontSize="25px" />
-              <Badge>{10}</Badge>
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu style={{ minWidth: 370 }}>
-              <span style={{ padding: 10 }}>Cart is empty</span>
-            </Dropdown.Menu>
-          </Dropdown>
-        </Nav>
-      </Container>
-    </Navbar>
+    <div className=" row center block">
+      <div>
+        <a href="#/">
+          <h2>Simple Shopping Cart</h2>
+        </a>
+      </div>
+      <div>
+        <a href="#/cart">Cart</a> <a href="#/signin">Sign Up</a>
+      </div>
+    </div>
   );
 };
 
