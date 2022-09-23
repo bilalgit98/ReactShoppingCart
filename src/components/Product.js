83291db1ff3964b7d7343e8f05ccbb3c3bf7d630
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-09-23 22:10:45 +0100

    useEffect API fetch for products

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,23 +1,23 @@
-import { useContext } from 'react';
-import { ShoppingCartContext } from '../context/ShoppingCartProvider';
+import { useContext } from "react";
+import { ShoppingCartContext } from "../context/ShoppingCartProvider";
 
-const Product = ({ item, item: { image, name, price }, itemInCart }) => {
+const Product = ({ item, item: { image, title, price }, itemInCart }) => {
   const shoppingCartContext = useContext(ShoppingCartContext);
   const { addItem, removeItem } = shoppingCartContext;
 
   return (
-    <div className='card'>
-      <img className='small' src={image} alt={name} />
-      <h3>{name}</h3>
+    <div className="card">
+      <img className="small" src={image} alt={title} />
+      <h3>{title}</h3>
       <div>£{price}</div>
       <div>
         {itemInCart ? (
           <div>
-            <button onClick={() => removeItem(item)} className='remove'>
+            <button onClick={() => removeItem(item)} className="remove">
               -
             </button>
-            <span className='p-1'>{item.qty}</span>
-            <button onClick={() => addItem(item)} className='add'>
+            <span className="p-1">{item.qty}</span>
+            <button onClick={() => addItem(item)} className="add">
               +
             </button>
           </div>
--- src/context/ShoppingCartProvider.js
+++ src/context/ShoppingCartProvider.js
@@ -1,33 +1,53 @@
-import { useState, createContext } from 'react';
-import products from '../data/products';
+import { useEffect, useState, createContext } from "react";
+// import products from '../data/products';
 
 export const ShoppingCartContext = createContext();
 
-const ShoppingCartProvider = props => {
+const ShoppingCartProvider = (props) => {
   const [items, setItems] = useState([]);
+  const [products, setProducts] = useState([]);
 
-  const addItem = item => {
-    const inCart = items.some(x => x.id === item.id);
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products?limit=5")
+      .then((response) => response.json())
+      .then((result) => {
+        setProducts(result);
+        console.log(result);
+      });
+  }, []);
+
+  const addItem = (item) => {
+    const inCart = items.some((x) => x.id === item.id);
     if (inCart) {
-      const updated = items.map(x => (x.id === item.id ? { ...x, quantity: x.quantity + 1 } : x));
+      const updated = items.map((x) =>
+        x.id === item.id ? { ...x, quantity: x.quantity + 1 } : x
+      );
       setItems(updated);
     } else {
       setItems([...items, { ...item, quantity: 1 }]);
     }
   };
 
-  const removeItem = item => {
-    const inCartItem = items.find(x => x.id === item.id);
+  const removeItem = (item) => {
+    const inCartItem = items.find((x) => x.id === item.id);
     if (inCartItem.quantity === 1) {
-      const updated = items.filter(x => x.id !== item.id);
+      const updated = items.filter((x) => x.id !== item.id);
       setItems(updated);
     } else {
-      const updated = items.map(x => (x.id === item.id ? { ...x, quantity: x.quantity - 1 } : x));
+      const updated = items.map((x) =>
+        x.id === item.id ? { ...x, quantity: x.quantity - 1 } : x
+      );
       setItems(updated);
     }
   };
 
-  return <ShoppingCartContext.Provider value={{ products, items, addItem, removeItem }}>{props.children}</ShoppingCartContext.Provider>;
+  return (
+    <ShoppingCartContext.Provider
+      value={{ products, items, addItem, removeItem }}
+    >
+      {props.children}
+    </ShoppingCartContext.Provider>
+  );
 };
 
 export default ShoppingCartProvider;
